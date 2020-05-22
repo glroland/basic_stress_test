e9aeb854cd736f0f950ef1d2c2c9a19d2a4bc4e2
Lee Roland <glroland@hotmail.com>
2020-05-22 11:14:51 -0400

    fresh off the generator

---
+++ stress-svc/src/main/java/com/glroland/stresssvc/StressSvcApplication.java
@@ -0,0 +1,13 @@
+package com.glroland.stresssvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StressSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StressSvcApplication.class, args);
+	}
+
+}
---
+++ stress-svc/src/test/java/com/glroland/stresssvc/StressSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glroland.stresssvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StressSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
