ded2bc800c26d138f95d9826999939c547a47ccb
Lee Roland <lroland@Lees-MBP.home.glroland.com>
2020-05-24 18:58:50 -0400

    added host ip address info to default http request

--- stress-svc/src/main/java/com/glroland/stress/DefaultServiceController.java
+++ stress-svc/src/main/java/com/glroland/stress/DefaultServiceController.java
@@ -2,12 +2,28 @@ package com.glroland.stress;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 public class DefaultServiceController {
 
     @RequestMapping("/")
     public String index() {
-        return "Stress Me Out!";
+        StringBuilder response = new StringBuilder();
+
+        response.append("Stress Me Out!!!\n");
+        response.append("\n");
+        response.append("Hostname: ");
+        try
+        {
+            response.append(InetAddress.getLocalHost().getHostAddress()).append("\n");
+        }
+        catch(UnknownHostException e)
+        {
+            response.append("Caught UnknownHostException\n").append(e.toString()).append("\n");
+        }
+
+        return response.toString();
     }
 }
